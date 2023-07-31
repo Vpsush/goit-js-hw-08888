c0f6b4c4bf65c547ca687b52e72addbc943be28f
Vpsush <vpsush2023@gmail.com>
2023-07-31 23:41:27 +0300

    ggh

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,43 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items";
+// Change code below this line
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+console.log(galleryItems);
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkUp = createGalleryItemsLibrary(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkUp);
+
+function createGalleryItemsLibrary(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join("");
+}
+
+galleryContainer.addEventListener("click", handleImageClick);
+
+function handleImageClick(event) {
+  event.preventDefault();
+  if (event.target.classList.contains("gallery__image")) {
+    const originalImageSrc = event.target.dataset.source;
+    const instance = basicLightbox.create(`
+      <img width="1280" height="auto" src="${originalImageSrc}">
+    `);
+    instance.show();
+  }
+}
